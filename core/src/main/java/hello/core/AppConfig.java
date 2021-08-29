9f6466280c24157fc89cf25bf8f125dbe9956ad1
HyeokChan <qpjiq123@gmail.com>
2021-08-29 21:21:06 +0900

    AppConfig 추가 및 생성자를 통한 의존성 주입

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,2 +1,31 @@
-package hello.core;public class AppConfig {
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        /*return new FixDiscountPolicy();*/
+        /*정률 할인 정책으로 변경*/
+        return new RateDiscountPolicy();
+    }
 }
