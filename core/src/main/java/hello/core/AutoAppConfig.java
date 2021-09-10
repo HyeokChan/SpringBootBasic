3e00bdaddefff9293cdf9c372cfdbd2db00a9a79
HyeokChan <qpjiq123@gmail.com>
2021-09-11 01:25:55 +0900

    스프링 어노테이션 설정 및 추가

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
+        basePackageClasses = AutoAppConfig.class,
         // AppConfig 예제를 남겨두기 위해 필터 설정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
+
+/*지정하지 않으면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작위치가 된다.*/
 public class AutoAppConfig {
 
 }
