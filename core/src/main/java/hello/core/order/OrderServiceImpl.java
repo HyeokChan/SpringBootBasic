09d415be120a71e213d5b302f98410ebdb4b0e5d
HyeokChan <qpjiq123@gmail.com>
2021-09-11 18:04:06 +0900

    어노테이션을 통한 의존성 주입, @Qualifier, @Primary

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+
+// @RequiredArgsConstructor : final 키워드가 붙은 객체를 가지는 생성자 만들어줌
 @Component
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     /*private final MemberRepository memberRepository = new MemoryMemberRepository();*/
     /*private final DiscountPolicy discountPolicy = new FixDiscountPolicy();*/
@@ -16,6 +21,8 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     // 생성자주입은 객체가 빈으로 등록될 때 의존 관계 설정도 이루어진다.
+    // 의존성 주입을 위해 빈을 찾을 때, 클래스명으로 먼저 검색하고 클래스가 여러개이면 여러개중에 필드명으로 찾는다.
+    // @Qualifier :  추가 구분자
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("memberRepository = " + memberRepository);
