4a895406f29df07d27665cf95b50834a89881e91
HyeokChan <qpjiq123@gmail.com>
2021-10-10 03:07:09 +0900

    README.md 수정중

--- core/src/main/java/hello/core/web/LogDemoService.java
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -12,6 +12,6 @@ public class LogDemoService {
     private final MyLogger myLogger;
 
     public void logic(String id) {
-        myLogger.log("service id = " + id); 
+        myLogger.log("service id = " + id);
     }
 }
